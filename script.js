c690d68f7ac08432ddb37ad0d6fa1c81791baa6c
Cynthia Nguyen <cynthia@Cynthias-MacBook-Pro.local>
2018-09-30 11:14:18 -0400

    added code for backup when diver hits wall

--- script.js
+++ script.js
@@ -2,50 +2,43 @@ let char = document.querySelector('#character')
 let leftPosition = 375;
 let topPosition = 210;
 let distance = 10;
-let wall = false;
 
 // console.log(getComputedStyle(document.querySelector('.v-wall')).getPropertyValue("left"))
 //console.log(document.querySelector('.v-wall').style.left)
 window.addEventListener("keydown", move)
 
 function move(event) {
-   if (event.keyCode == 39 && wall == false){
-     collision()
-     if(wall == false){
-     leftPosition += distance;
-     char.style.left = leftPosition + 'px';}
-   } else if (event.keyCode == 37 && wall == false) {
-     collision ()
-     if(wall == false){
-     leftPosition += -distance;
-     char.style.left = leftPosition + 'px';}
-   } else if (event.keyCode == 38 && wall == false) {
-    collision()
-    if(wall == false){
+  collision()
+  if (event.keyCode == 39){
+    leftPosition += distance;
+    char.style.left = leftPosition + 'px';
+    distance = 10
+  } else if (event.keyCode == 37) {
+    leftPosition += -distance;
+    char.style.left = leftPosition + 'px';
+  } else if (event.keyCode == 38) {
     topPosition += -distance;
-     char.style.top = topPosition + 'px';}
-  } else if (event.keyCode == 40 && wall == false) {
-    collision()
-    if(wall == false){
+    char.style.top = topPosition + 'px';
+  } else if (event.keyCode == 40) {
     topPosition += distance;
     char.style.top = topPosition + 'px';
+    char.rotate
   }
-wall = false
-}}
+}
 
 function collision() {
 char.innerHTML = ` L: ${leftPosition} px<br>T: ${topPosition} px `;
 if (topPosition > 375 && leftPosition >= 1000){
   alert('YOU WIN');
 } else if (topPosition < 210 ||
-  topPosition > 560 ||
-  leftPosition > 565 && leftPosition < 620 && topPosition < 410 ||
+  topPosition > 540 ||
+  leftPosition > 515 && leftPosition < 620 && topPosition < 410 ||
   leftPosition > 770 && leftPosition < 825 && topPosition > 355 ||
   leftPosition < 420 && topPosition > 355 ||
   leftPosition > 970 && topPosition < 410
   ) {
-  alert('thats a wall dummy')
-wall = true
+  alert("That's a wall, silly!")
+  distance = -10
 }}
 
 
